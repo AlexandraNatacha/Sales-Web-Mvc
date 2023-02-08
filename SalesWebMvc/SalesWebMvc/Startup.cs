7fe3a38117d02edbdc4a6f4e91b1f83760e2eac3
Alexandra Natacha <alextachaa@gmail.com>
2023-02-08 12:08:40 -0400

    Adicionei o pacote Razor.RuntimeCompilation

--- SalesWebMvc/SalesWebMvc/Startup.cs
+++ SalesWebMvc/SalesWebMvc/Startup.cs
@@ -29,7 +29,7 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySQL(Configuration.GetConnectionString("SalesWebMvcContext"), builder => builder.MigrationsAssembly("SalesWebMvc")));
